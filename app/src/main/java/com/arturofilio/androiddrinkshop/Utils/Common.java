080cdb15c5f4718d2c446d19b498d15282b4c0e5
Arturo Filio Villa <arturfil@hotmail.com>
2018-08-01 02:45:13 -0400

    changed the ip address to fix the bug in the register layout

--- app/src/main/java/com/arturofilio/androiddrinkshop/Utils/Common.java
+++ app/src/main/java/com/arturofilio/androiddrinkshop/Utils/Common.java
@@ -5,7 +5,7 @@ import com.arturofilio.androiddrinkshop.Retrofit.RetrofitClient;
 
 public class Common {
     //In Emulator, localhost = 127.0.0.1
-    private static final String BASE_URL = "http://127.0.0.1/drinkshop/";
+    private static final String BASE_URL = "http://192.168.1.7/drinkshop/";
 
     public static IDrinkShopApi getApi(){
         return RetrofitClient.getClient(BASE_URL).create(IDrinkShopApi.class);
